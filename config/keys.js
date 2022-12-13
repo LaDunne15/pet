06b9e31646c1c75d31a07831eb6f93899ba9f44c
LaDunne15 <vladilsav7772@gmail.com>
2022-12-13 23:21:24 +0200

    com55

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
     //mongoURI: 'mongodb://127.0.0.1:27017/empty_db?retryWrites=true&w=majority',
     //localURI: 'http://localhost:3000',
-    localURI:'https://verdant-toffee-c30c46.netlify.app',
+    localURI:'https://pet-ruddy.vercel.app',
     mongoURI:'mongodb+srv://test_user:test_pass@cluster0.mxosg.mongodb.net/empty_pet?retryWrites=true&w=majority',
     jwt: 'dev-jwt'
 }
