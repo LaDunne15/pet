cbac6c4b1334c8d9790dd4cf89a0478b6a636ad9
LaDunne15 <vladilsav7772@gmail.com>
2022-12-17 21:06:53 +0200

    bug_not_avoid

--- src/components/cabinet/CabinetForm.vue
+++ src/components/cabinet/CabinetForm.vue
@@ -1,107 +1,28 @@
-<template>
-    <div>
-      <p>Кабінет користувача</p>
-      <table>
-        <tr>
-          <td>Фото:</td>
-          <td>
-            <div v-for="img in user.images" :key="img._id">
-              <img :src="'data:'+img.contentType+';base64,'+img.data">
-            </div>
-            <!--<img style="width: 100%" :src="'data:'+main_img.contentType+';base64,'+main_img.data">-->
-          </td>
-          <td><input type="file" @change=uploadImage name="image">
-            <button v-on:click="send">Завантажити</button></td>
-        </tr>
-        <tr>
-          <td>ID:</td>
-          <td>{{user._id}}</td>
-        </tr>
-        <tr>
-          <td>Пошта:</td>
-          <td>{{user.email}}</td>
-        </tr>
-        <hr/>
-        <tr>
-          <td>Ім'я:</td>
-          <td v-if="!changeMod">{{user.firstname}}</td>
-          <td v-if="changeMod"><input type="text" v-model="user.firstname"></td>
-        </tr>
-        <tr>
-          <td>Прізвище:</td>
-          <td v-if="!changeMod">{{user.lastname}}</td>
-          <td v-if="changeMod"><input type="text" v-model="user.lastname"></td>
-        </tr>
-        <tr>
-          <td>По батькові:</td>
-          <td v-if="!changeMod">{{user.fathername}}</td>
-          <td v-if="changeMod"><input type="text" v-model="user.fathername"></td>
-        </tr>
-        <tr>
-          <td>Дата народження:</td>
-          <td v-if="!changeMod">{{dateB}}</td>
-          <td v-if="changeMod"><input type="date" v-model="dateB" max="2010-01-01" min="1910-01-01" default="2000-01-01" required>
-      </td>
-        </tr>
-        <tr>
-          <td><button v-if="changeMod" @click="changeMod=false">Cкасувати</button></td>
-          <td><button v-if="!changeMod" @click="changeMod=true">Змінити</button>
-          <button v-if="changeMod" @click="save">Зберегти</button></td>
-        </tr>
-        <hr/>
-        <tr>
-          <td>Пароль:</td>
-          <td v-if="!changePasswordMod">*****</td>
-          <td v-if="changePasswordMod"><input type="text" v-model="pass"></td>
-        </tr>
-        <tr v-if="changePasswordMod">
-          <td></td>
-          <td><input type="text" v-model="pass2"></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td v-if="(msgs.length!=0)">
-          <p  v-for="msg in msgs" :key="msg">
-          {{msg}}
-          </p>
-          </td>
-        </tr>
-        <tr>
-          <td><button v-if="changePasswordMod" @click="changePasswordMod=false">Cкасувати</button></td>
-          <td>
-            <button v-if="!changePasswordMod" @click="changePasswordMod=true">Змінити</button>
-            <button v-if="changePasswordMod" @click="savePass">Змінити пароль</button>
-          </td>
-        </tr>
-      </table>
-    </div>
-  </template>
-  
 <script>
-//import store from '../state/state'
 import axios from 'axios'
 const keys = require('../../../config/keys');
-  export default {
-    name: 'CabinetComponent',
-    props: {
-      id: String
-    },
-    data() {
-      return {
-        jwt:"",
-        user: [],
-        changeMod: false,
-        changePasswordMod: false,
-        dateB: "",
-        pass: "",
-        pass2: "",
-        msgs: [],
-        file: '',
-        main_img: []
-      }
-    },
-    methods: {
-      save: async function () {        
+import ImageForm from '@/components/img/ImageForm.vue';
+export default {
+  name: 'CabinetComponent',
+  props: {
+    id: String
+  },
+  data() {
+    return {
+      jwt:"",
+      user: [],
+      changeMod: false,
+      changePasswordMod: false,
+      dateB: "",
+      pass: "",
+      pass2: "",
+      msgs: [],
+      file: '',
+      main_img: []
+    }
+  },
+  methods: {
+    save: async function () {        
         var params = {
           firstname: this.user.firstname,
           lastname: this.user.lastname,
@@ -173,9 +94,90 @@ const keys = require('../../../config/keys');
     {
       console.log("Не авторизовано");
     }
-    }
-  }
-  </script>
-  <style scoped>
-  </style>
+    },
+    components: {ImageForm}
+}
+</script>
+<template>
+    <div>
+      <p>Кабінет користувача</p>
+      <table>
+        <tr>
+          <td>Фото:</td>
+          <td>
+            <!--
+            <div v-for="img in user.images" :key="img._id">
+              <ImageForm style="width: 100%" :data="img"></ImageForm>
+            </div>
+            -->
+            <ImageForm style="width: 100%" :data="main_img"></ImageForm>
+          </td>
+          <td><input type="file" @change=uploadImage name="image">
+            <button v-on:click="send">Завантажити</button></td>
+        </tr>
+        <tr>
+          <td>ID:</td>
+          <td>{{user._id}}</td>
+        </tr>
+        <tr>
+          <td>Пошта:</td>
+          <td>{{user.email}}</td>
+        </tr>
+        <hr/>
+        <tr>
+          <td>Ім'я:</td>
+          <td v-if="!changeMod">{{user.firstname}}</td>
+          <td v-if="changeMod"><input type="text" v-model="user.firstname"></td>
+        </tr>
+        <tr>
+          <td>Прізвище:</td>
+          <td v-if="!changeMod">{{user.lastname}}</td>
+          <td v-if="changeMod"><input type="text" v-model="user.lastname"></td>
+        </tr>
+        <tr>
+          <td>По батькові:</td>
+          <td v-if="!changeMod">{{user.fathername}}</td>
+          <td v-if="changeMod"><input type="text" v-model="user.fathername"></td>
+        </tr>
+        <tr>
+          <td>Дата народження:</td>
+          <td v-if="!changeMod">{{dateB}}</td>
+          <td v-if="changeMod"><input type="date" v-model="dateB" max="2010-01-01" min="1910-01-01" default="2000-01-01" required>
+      </td>
+        </tr>
+        <tr>
+          <td><button v-if="changeMod" @click="changeMod=false">Cкасувати</button></td>
+          <td><button v-if="!changeMod" @click="changeMod=true">Змінити</button>
+          <button v-if="changeMod" @click="save">Зберегти</button></td>
+        </tr>
+        <hr/>
+        <tr>
+          <td>Пароль:</td>
+          <td v-if="!changePasswordMod">*****</td>
+          <td v-if="changePasswordMod"><input type="text" v-model="pass"></td>
+        </tr>
+        <tr v-if="changePasswordMod">
+          <td></td>
+          <td><input type="text" v-model="pass2"></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td v-if="(msgs.length!=0)">
+          <p  v-for="msg in msgs" :key="msg">
+          {{msg}}
+          </p>
+          </td>
+        </tr>
+        <tr>
+          <td><button v-if="changePasswordMod" @click="changePasswordMod=false">Cкасувати</button></td>
+          <td>
+            <button v-if="!changePasswordMod" @click="changePasswordMod=true">Змінити</button>
+            <button v-if="changePasswordMod" @click="savePass">Змінити пароль</button>
+          </td>
+        </tr>
+      </table>
+    </div>
+</template>
+<style scoped>
+</style>
   
---
+++ src/components/img/ImageForm.vue
@@ -0,0 +1,10 @@
+<template>
+    <img v-if="data.data" :src="'data:'+data.contentType+';base64,'+data.data">
+</template>
+<script>
+export default {
+    name: 'ImageForm',
+    props: [ 'data' ]
+}
+</script>
+  
